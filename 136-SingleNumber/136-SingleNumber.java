41ff5e8d3dc34741a0513a861aab370c0c08ce36
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:42:30 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,10 @@
+// Last updated: 8/7/2025, 11:38:28 AM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int v : nums) {
+            ans ^= v;
+        }
+        return ans;
+    }
+}
