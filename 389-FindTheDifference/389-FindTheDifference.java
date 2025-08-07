c7a8567786d0226d8af476fea3abdfed55e8a0c4
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:41:28 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,14 @@
+// Last updated: 8/7/2025, 11:37:26 AM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            ++cnt[s.charAt(i) - 'a'];
+        }
+        for (int i = 0;; ++i) {
+            if (--cnt[t.charAt(i) - 'a'] < 0) {
+                return t.charAt(i);
+            }
+        }
+    }
+}
