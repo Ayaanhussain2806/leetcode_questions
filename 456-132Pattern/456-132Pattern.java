301dbec6b057246cba510fc0b27be3ef01e97c84
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:41:17 +0530

    Create 456-132Pattern

---
+++ 456-132Pattern/456-132Pattern.java
@@ -0,0 +1,17 @@
+// Last updated: 8/7/2025, 11:37:15 AM
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int vk = -(1 << 30);
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (nums[i] < vk) {
+                return true;
+            }
+            while (!stk.isEmpty() && stk.peek() < nums[i]) {
+                vk = stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
+    }
+}
