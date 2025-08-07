2effc01ce1035ae4a67acfaec0f80038a357ed22
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:41:39 +0530

    Create 342-PowerOfFour

---
+++ 342-PowerOfFour/342-PowerOfFour.java
@@ -0,0 +1,20 @@
+// Last updated: 8/7/2025, 11:37:37 AM
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        while(n!=1){
+            if(n%4!=0){
+                return false;
+            }
+            else{
+                n=n/4;
+            }
+        }
+        return true;
+    }
+}
