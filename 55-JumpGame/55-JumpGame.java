e9639b9976a1b6908da3e3bcfcca19cd947b2edc
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:43:04 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,13 @@
+// Last updated: 8/7/2025, 11:39:02 AM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int mx = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (mx < i) {
+                return false;
+            }
+            mx = Math.max(mx, i + nums[i]);
+        }
+        return true;
+    }
+}
