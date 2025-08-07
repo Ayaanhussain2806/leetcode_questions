719249296c0eac0d4a7c080c5205fed4cd07d47e
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:41:53 +0530

    Create 279-PerfectSquares

---
+++ 279-PerfectSquares/279-PerfectSquares.java
@@ -0,0 +1,15 @@
+// Last updated: 8/7/2025, 11:37:51 AM
+class Solution {
+    public int numSquares(int n) {
+        int m = (int) Math.sqrt(n);
+        int[] f = new int[n + 1];
+        Arrays.fill(f, 1 << 30);
+        f[0] = 0;
+        for (int i = 1; i <= m; ++i) {
+            for (int j = i * i; j <= n; ++j) {
+                f[j] = Math.min(f[j], f[j - i * i] + 1);
+            }
+        }
+        return f[n];
+    }
+}
