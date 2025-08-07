e11e98f9098f949be74d63af84a3cb8a75f34a06
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-07 11:40:21 +0530

    Create 1884-MinimumChangesToMakeAlternatingBinaryString

---
+++ 1884-MinimumChangesToMakeAlternatingBinaryString/1884-MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,10 @@
+// Last updated: 8/7/2025, 11:36:19 AM
+class Solution {
+    public int minOperations(String s) {
+        int cnt = 0, n = s.length();
+        for (int i = 0; i < n; ++i) {
+            cnt += (s.charAt(i) != "01".charAt(i & 1) ? 1 : 0);
+        }
+        return Math.min(cnt, n - cnt);
+    }
+}
