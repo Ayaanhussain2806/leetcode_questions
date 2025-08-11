664dd0bbb194311834fca28364be7dc3fab3cf79
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-08-11 14:21:17 +0530

    File updated at 8/11/2025, 2:17:12 PM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,24 +1,24 @@
-// Last updated: 8/11/2025, 1:16:50 PM
+// Last updated: 8/11/2025, 2:17:12 PM
 class Solution {
-    public int trap(int[] height) {
-        return Trapping(height);
+    public int[] productExceptSelf(int[] nums) {
+        return Product(nums);
     }
-    public static int Trapping(int[] height){
-        int n=height.length;
+    public static int[] Product(int[] nums){
+        int n=nums.length;
         int[] left=new int[n];
         int[] right=new int[n];
-        left[0]=height[0];
+        left[0]=1;
         for(int i=1;i<n;i++){
-            left[i]=Math.max(left[i-1],height[i]);
+            left[i]=left[i-1]*nums[i-1];
         }
-        right[n-1]=height[n-1];
+        right[n-1]=1;
         for(int i=n-2;i>=0;i--){
-            right[i]=Math.max(right[i+1],height[i]);
+            right[i]=right[i+1]*nums[i+1];
         }
-        int sum=0;
+        int[] product=new int[n];
         for(int i=0;i<n;i++){
-            sum+=Math.min(left[i],right[i])-height[i];
+            product[i]=left[i]*right[i];
         }
-        return sum;
+        return product;
     }
 }
