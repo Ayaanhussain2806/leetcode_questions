5a37dd8d326e9340b081d252c4ad744623f68bb8
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-03 11:38:19 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,24 @@
+// Last updated: 9/3/2025, 11:34:00 AM
+
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return robber(nums, 0, dp);
+    }
+
+    public static int robber(int[] nums, int i, int[] dp) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int rob = nums[i] + robber(nums, i + 2, dp);
+        int dontRob = robber(nums, i + 1, dp);
+
+        dp[i] = Math.max(rob, dontRob);
+        return dp[i];
+    }
+}
