7ebe77e248cfd8938fbc6210d564d4fa27a3c068
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-17 15:54:14 +0530

    File updated at 9/17/2025, 3:49:46 PM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,13 +1,12 @@
-// Last updated: 8/7/2025, 11:37:54 AM
+// Last updated: 9/17/2025, 3:49:46 PM
 class Solution {
     public int missingNumber(int[] nums) {
         int sum=0;
         for(int i=0;i<nums.length;i++){
             sum+=nums[i];
         }
-        int actual_sum=(nums.length*(nums.length+1))/2;
-        int n=actual_sum-sum;
-        return n;
+        int actual_sum=0;
+        actual_sum=nums.length*(nums.length+1)/2;
+        return actual_sum-sum;
     }
-    
 }
