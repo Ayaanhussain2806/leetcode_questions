a692860d429dbe188bb574af5ec17250b36a893f
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-19 03:24:42 +0530

    File updated at 9/19/2025, 3:20:13 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,36 +1,27 @@
-// Last updated: 9/19/2025, 3:03:58 AM
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums); 
+// Last updated: 9/19/2025, 3:20:13 AM
+class MinStack {
+    private Deque<Integer> stk1 = new ArrayDeque<>();
+    private Deque<Integer> stk2 = new ArrayDeque<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                }
-                else if (sum < 0) {
-                    left++; 
-                }
-                else {
-                    right--; 
-                }
-            }
-        }
-        return result;
+    public MinStack() {
+        stk2.push(Integer.MAX_VALUE);
     }
 
+    public void push(int val) {
+        stk1.push(val);
+        stk2.push(Math.min(val, stk2.peek()));
+    }
+
+    public void pop() {
+        stk1.pop();
+        stk2.pop();
+    }
+
+    public int top() {
+        return stk1.peek();
+    }
+
+    public int getMin() {
+        return stk2.peek();
+    }
 }
