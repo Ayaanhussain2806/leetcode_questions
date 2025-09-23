362419ce9e12b1e97388a5cf8a05b3eff28194f9
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-23 15:40:50 +0530

    File updated at 9/23/2025, 3:36:16 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,17 +1,20 @@
-// Last updated: 8/7/2025, 11:36:56 AM
+// Last updated: 9/23/2025, 3:36:16 PM
 class Solution {
-    public int numSubarrayProductLessThanK(int[] arr, int k) {
-        int ans=0,si=0,ei=0,p=1;
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        return max_pro(nums,k);
+    }
+    public static int max_pro(int[] arr,int k){
+        int si=0;
+        int ei=0;
+        int p=1;
+        int ans=0;
         while(ei<arr.length){
-            //grow
-            p=p*arr[ei];
-            //shrink
+            p*=arr[ei];
             while(p>=k && si<=ei){
-                p=p/arr[si];
+                p/=arr[si];
                 si++;
             }
-            //ans update
-            ans=ans+(ei-si+1);
+            ans= ans+(ei-si+1);
             ei++;
         }
         return ans;
