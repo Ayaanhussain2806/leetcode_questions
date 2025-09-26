30caf664fbf84a849b8a33bac9169f773ba552ec
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-26 21:56:33 +0530

    Create 997-FindtheTownJudge

---
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -0,0 +1,19 @@
+// Last updated: 9/26/2025, 9:51:57 PM
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegree=new int[n+1];
+        int[] outdegree=new int[n+1];
+        for(int[] arr:trust){
+            int a=arr[0];
+            int b=arr[1];
+            outdegree[a]++;
+            indegree[b]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(indegree[i]==n-1 && outdegree[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
