5bea0ddd873998341dc3fcd7fd318d39d792b77c
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-27 13:14:43 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.java
@@ -0,0 +1,9 @@
+// Last updated: 9/27/2025, 1:10:05 PM
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int c= nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3];
+        int d= nums[0]*nums[1]*nums[nums.length-1];
+        return Math.max(c,d);
+    }
+}
