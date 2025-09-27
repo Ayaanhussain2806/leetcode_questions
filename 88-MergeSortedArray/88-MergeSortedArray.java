49ce61390433f2fd04006b4b0fc24b235ade85b4
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-27 22:02:56 +0530

    File updated at 9/27/2025, 9:58:19 PM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,9 +1,7 @@
-// Last updated: 9/27/2025, 9:53:39 PM
+// Last updated: 9/27/2025, 9:58:19 PM
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[i+m]=nums2[i];
-        }
-        Arrays.sort(nums1);
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
     }
 }
