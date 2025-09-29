277bc5487c49cd2a4e2958dde0db08f5f67fa44d
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-29 21:44:01 +0530

    Create 74-Searcha2DMatrix

---
+++ 74-Searcha2DMatrix/74-Searcha2DMatrix.java
@@ -0,0 +1,13 @@
+// Last updated: 9/29/2025, 9:39:23 PM
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
