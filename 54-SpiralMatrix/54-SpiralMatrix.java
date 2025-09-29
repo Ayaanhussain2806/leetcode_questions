b1f0f06f7ee61a1d3b78e23a91ec28a5088906c9
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-29 23:01:07 +0530

    File updated at 9/29/2025, 10:56:29 PM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,35 +1,30 @@
-// Last updated: 9/29/2025, 10:31:10 PM
+// Last updated: 9/29/2025, 10:56:29 PM
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> ans=new ArrayList<>();
-        int minc=0;
-        int minr=0;
-        int maxc=matrix[0].length-1;
-        int maxr=matrix.length-1;
-        int te=matrix.length*matrix[0].length;
-        int c=0;
-        while(c<te){
-            for(int i=minc;i<=maxc && c<te;i++){
-                ans.add(matrix[minr][i]);
-                c++;
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j); 
+                }
             }
-            minr++;
-            for(int i=minr;i<=maxr && c<te;i++){
-                ans.add(matrix[i][maxc]);
-                c++;
-            }
-            maxc--;
-            for(int i=maxc;i>=minc && c<te;i--){
-                ans.add(matrix[maxr][i]);
-                c++;
-            }
-            maxr--;
-            for(int i=maxr;i>=minr&& c<te;i--){
-                ans.add(matrix[i][minc]);
-                c++;
-            }
-            minc++;
         }
-        return ans;
+        return count;
+    }
+
+    public void dfs(char[][] grid, int i, int j) {
+        
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        
+        grid[i][j] = '0'; 
+
+        
+        dfs(grid, i + 1, j); 
+        dfs(grid, i - 1, j); 
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1); 
     }
 }
