27cf597b844546d89a179d68be40d71e74b47078
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-29 22:35:49 +0530

    File updated at 9/29/2025, 10:31:10 PM

--- 54-SpiralMatrix/54-SpiralMatrix.java
+++ 54-SpiralMatrix/54-SpiralMatrix.java
@@ -1,44 +1,35 @@
-// Last updated: 8/7/2025, 11:39:03 AM
+// Last updated: 9/29/2025, 10:31:10 PM
 class Solution {
-    public List<Integer> spiralOrder(int[][] arr) {
-        return Spiral(arr); // Return the result from Spiral
-    }
-
-    public static List<Integer> Spiral(int[][] arr) {
-        List<Integer> result = new ArrayList<>(); // List to store the spiral order
-        int minc = 0, minr = 0, maxc = arr[0].length - 1, maxr = arr.length - 1;
-        int te = arr.length * arr[0].length; // Total elements
-        int c = 0;
-        while (c < te) {
-            // Traverse from left to right
-            for (int i = minc; i <= maxc && c < te; i++) {
-                result.add(arr[minr][i]); // Add element to result list
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans=new ArrayList<>();
+        int minc=0;
+        int minr=0;
+        int maxc=matrix[0].length-1;
+        int maxr=matrix.length-1;
+        int te=matrix.length*matrix[0].length;
+        int c=0;
+        while(c<te){
+            for(int i=minc;i<=maxc && c<te;i++){
+                ans.add(matrix[minr][i]);
                 c++;
             }
             minr++;
-
-            // Traverse from top to bottom
-            for (int i = minr; i <= maxr && c < te; i++) {
-                result.add(arr[i][maxc]);
+            for(int i=minr;i<=maxr && c<te;i++){
+                ans.add(matrix[i][maxc]);
                 c++;
             }
             maxc--;
-
-            // Traverse from right to left
-            for (int i = maxc; i >= minc && c < te; i--) {
-                result.add(arr[maxr][i]);
+            for(int i=maxc;i>=minc && c<te;i--){
+                ans.add(matrix[maxr][i]);
                 c++;
             }
             maxr--;
-
-            // Traverse from bottom to top
-            for (int i = maxr; i >= minr && c < te; i--) {
-                result.add(arr[i][minc]);
+            for(int i=maxr;i>=minr&& c<te;i--){
+                ans.add(matrix[i][minc]);
                 c++;
             }
             minc++;
         }
-
-        return result; // Return the list of elements in spiral order
+        return ans;
     }
 }
