2ed58559c9633e1cd5906e61f5d0e42867b9b3b9
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-01 15:37:02 +0530

    File updated at 10/1/2025, 3:32:22 PM

--- 3340-CheckBalancedString/3340-CheckBalancedString.java
+++ 3340-CheckBalancedString/3340-CheckBalancedString.java
@@ -1,20 +1,14 @@
-// Last updated: 10/1/2025, 3:27:43 PM
+// Last updated: 10/1/2025, 3:32:22 PM
 class Solution {
     public boolean isBalanced(String num) {
-        int s1=0;
-        int s2=0;
-        for(int i=0;i<num.length();i++){
-            if(i%2!=0){
-                int c=num.charAt(i);
-                s1+=c-'0';
-            }else{
-                int d=num.charAt(i);
-                s2+=d-'0';
-            }
+        int sum1=0;
+        int sum2=0;
+        for(int i=0;i<num.length();i+=2){
+            sum1+=num.charAt(i)-'0';
         }
-        if(s1==s2){
-            return true;
+        for(int j=1;j<num.length();j+=2){
+            sum2=sum2+(num.charAt(j)-'0');
         }
-        return false;
+        return sum1==sum2;
     }
 }
