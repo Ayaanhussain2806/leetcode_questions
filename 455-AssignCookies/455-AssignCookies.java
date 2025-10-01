c60c5361c0f60f4e0a27a9b641158cbd61cd6755
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-01 11:25:21 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,20 @@
+// Last updated: 10/1/2025, 11:20:41 AM
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0;
+        int j=0;
+        int count=0;
+        while(i<g.length && j<s.length){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+    }
+}
