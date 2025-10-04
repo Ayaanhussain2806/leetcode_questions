9bbe5a666dd7ca91f00e5303ff37e9e1b7527e1c
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-04 13:31:29 +0530

    File updated at 10/4/2025, 1:26:45 PM

--- 90-SubsetsII/90-SubsetsII.java
+++ 90-SubsetsII/90-SubsetsII.java
@@ -1,20 +1,20 @@
-// Last updated: 10/4/2025, 1:20:30 PM
+// Last updated: 10/4/2025, 1:26:45 PM
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans= new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
         List<Integer> ll=new ArrayList<>();
-        solve(nums,ll,0,ans);
+        solve(nums,ans,0,ll);
         return ans;
     }
-    public static void solve(int[] nums,List<Integer> ll,int idx,List<List<Integer>> ans){
+    public static void solve(int[] nums, List<List<Integer>> ans,int idx,List<Integer> ll){
         ans.add(new ArrayList<>(ll));
         for(int i=idx;i<nums.length;i++){
             if(i>idx && nums[i]==nums[i-1]){
                 continue;
             }
             ll.add(nums[i]);
-            solve(nums,ll,i+1,ans);
+            solve(nums,ans,i+1,ll);
             ll.remove(ll.size()-1);
         }
     }
