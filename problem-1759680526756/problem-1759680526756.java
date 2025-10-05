b9062c803a34c742c6839341c6c7dcd688f8f81f
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-05 21:43:31 +0530

    Create

---
+++ problem-1759680526756/problem-1759680526756.java
@@ -0,0 +1,13 @@
+// Last updated: 10/5/2025, 9:38:46 PM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
