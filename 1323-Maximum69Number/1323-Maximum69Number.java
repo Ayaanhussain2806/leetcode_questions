7d9e9ca0f0ea3de505fc5161b06740ab5425596d
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-06 13:48:43 +0530

    File updated at 10/6/2025, 1:43:57 PM

--- 1323-Maximum69Number/1323-Maximum69Number.java
+++ 1323-Maximum69Number/1323-Maximum69Number.java
@@ -1,25 +1,47 @@
-// Last updated: 10/6/2025, 1:20:04 PM
+// Last updated: 10/6/2025, 1:43:57 PM
 class Solution {
-    public boolean predictTheWinner(int[] nums) {
-        int n = nums.length;
-        int[][] memo = new int[n][n];
-        // Initialize with a special value to indicate "uncomputed"
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                memo[i][j] = Integer.MIN_VALUE;
+    int result = 0;
+    int emptyCount = 1;
+
+    public int uniquePathsIII(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int startX = 0, startY = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    startX = i;
+                    startY = j;
+                } else if (grid[i][j] == 0) {
+                    emptyCount++;
+                }
             }
         }
-        return helper(nums, 0, n - 1, memo) >= 0;
+
+        dfs(grid, startX, startY);
+        return result;
     }
 
-    private int helper(int[] nums, int i, int j, int[][] memo) {
-        if (i == j) return nums[i];
-        if (memo[i][j] != Integer.MIN_VALUE) return memo[i][j];
+    public void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == -1) {
+            return;
+        }
 
-        int pickLeft = nums[i] - helper(nums, i + 1, j, memo);
-        int pickRight = nums[j] - helper(nums, i, j - 1, memo);
+        if (grid[i][j] == 2) {
+            if (emptyCount == 0) result++;
+            return;
+        }
 
-        memo[i][j] = Math.max(pickLeft, pickRight);
-        return memo[i][j];
+        grid[i][j] = -1;
+        emptyCount--;
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+
+        grid[i][j] = 0;
+        emptyCount++;
     }
 }
