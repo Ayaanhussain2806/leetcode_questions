1ad2d637b5a1c6ba7a95c756356fb910c238fd1e
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-09 00:50:40 +0530

    Create 3467-TransformArraybyParity

---
+++ 3467-TransformArraybyParity/3467-TransformArraybyParity.java
@@ -0,0 +1,15 @@
+// Last updated: 10/9/2025, 12:45:53 AM
+class Solution {
+    public int[] transformArray(int[] nums) {
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                arr[i]=0;
+            }else{
+                arr[i]=1;
+            }
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
