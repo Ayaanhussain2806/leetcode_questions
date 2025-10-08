4c8682b28460e2fbe31175105e15a7837fc7ea43
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-09 00:51:44 +0530

    File updated at 10/9/2025, 12:46:57 AM

--- 3467-TransformArraybyParity/3467-TransformArraybyParity.java
+++ 3467-TransformArraybyParity/3467-TransformArraybyParity.java
@@ -1,4 +1,4 @@
-// Last updated: 10/9/2025, 12:45:53 AM
+// Last updated: 10/9/2025, 12:46:57 AM
 class Solution {
     public int[] transformArray(int[] nums) {
         int[] arr=new int[nums.length];
@@ -9,7 +9,16 @@ class Solution {
                 arr[i]=1;
             }
         }
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
         return arr;
     }
 }
