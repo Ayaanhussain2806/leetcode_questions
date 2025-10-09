3088d245441a380d79ede910022ebae44753349a
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-09 23:51:23 +0530

    File updated at 10/9/2025, 11:46:34 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -1,20 +1,20 @@
-// Last updated: 10/9/2025, 11:46:17 PM
+// Last updated: 10/9/2025, 11:46:34 PM
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] arr=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             arr[i]=nums[i]*nums[i];
         }
-        // Arrays.sort(arr);
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                }
-            }
-        }
+         Arrays.sort(arr);
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(arr[i]>arr[j]){
+        //             int temp=arr[i];
+        //             arr[i]=arr[j];
+        //             arr[j]=temp;
+        //         }
+        //     }
+        // }
         return arr;
     }
 }
