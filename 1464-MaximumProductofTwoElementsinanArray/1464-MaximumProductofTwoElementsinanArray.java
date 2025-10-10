0cdb23d1b1f76445c4eca22f8d8f1635436a36d2
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-10 21:07:50 +0530

    File updated at 10/10/2025, 9:03:02 PM

--- 1464-MaximumProductofTwoElementsinanArray/1464-MaximumProductofTwoElementsinanArray.java
+++ 1464-MaximumProductofTwoElementsinanArray/1464-MaximumProductofTwoElementsinanArray.java
@@ -1,14 +1,11 @@
-// Last updated: 10/10/2025, 9:00:39 PM
+// Last updated: 10/10/2025, 9:03:02 PM
 class Solution {
     public int maxProduct(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i+1;j<nums.length;j++){
-                sum=(nums[i]-1)*(nums[j]-1);
-                max=Math.max(sum,max);
-            }
-        }
-        return max;
+        Arrays.sort(nums);
+        int i=nums.length-1;
+        int j=nums.length-2;
+
+        return (nums[i]-1)*(nums[j]-1);
+
     }
 }
