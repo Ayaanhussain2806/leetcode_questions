29fe5c5e6f5ac6b83cc5da5b66fba82ab7194425
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-11 21:18:17 +0530

    File updated at 10/11/2025, 9:13:29 PM

--- 3194-MinimumAverageofSmallestandLargestElements/3194-MinimumAverageofSmallestandLargestElements.java
+++ 3194-MinimumAverageofSmallestandLargestElements/3194-MinimumAverageofSmallestandLargestElements.java
@@ -1,13 +1,13 @@
-// Last updated: 10/11/2025, 9:12:54 PM
+// Last updated: 10/11/2025, 9:13:29 PM
 class Solution {
     public double minimumAverage(int[] nums) {
         int n=nums.length/2;
         double[] arr=new double[n];
         Arrays.sort(nums);
         for(int i=0;i<n;i++){
-            double sum=(nums[i]+nums[nums.length-i-1])/2.0;
-            arr[i]=sum;
-            sum=0;
+            arr[i]=(nums[i]+nums[nums.length-i-1])/2.0;
+            
+            
         }
         Arrays.sort(arr);
         return arr[0];
