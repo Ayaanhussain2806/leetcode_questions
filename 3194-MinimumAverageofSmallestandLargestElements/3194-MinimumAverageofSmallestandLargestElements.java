254bf2962ca62b290c244fcbcb489908b9165d47
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-11 21:17:42 +0530

    Create 3194-MinimumAverageofSmallestandLargestElements

---
+++ 3194-MinimumAverageofSmallestandLargestElements/3194-MinimumAverageofSmallestandLargestElements.java
@@ -0,0 +1,15 @@
+// Last updated: 10/11/2025, 9:12:54 PM
+class Solution {
+    public double minimumAverage(int[] nums) {
+        int n=nums.length/2;
+        double[] arr=new double[n];
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            double sum=(nums[i]+nums[nums.length-i-1])/2.0;
+            arr[i]=sum;
+            sum=0;
+        }
+        Arrays.sort(arr);
+        return arr[0];
+    }
+}
