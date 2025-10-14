25fabc752f024eaf22fb950d55517250225f3985
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-14 22:45:12 +0530

    File updated at 10/14/2025, 10:40:23 PM

--- 2418-SortthePeople/2418-SortthePeople.java
+++ 2418-SortthePeople/2418-SortthePeople.java
@@ -1,4 +1,4 @@
-// Last updated: 10/9/2025, 12:50:09 PM
+// Last updated: 10/14/2025, 10:40:23 PM
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         Object[][] people=new Object[names.length][2];
@@ -6,7 +6,7 @@ class Solution {
             people[i][0]=names[i];
             people[i][1]=heights[i];
         }
-        Arrays.sort(people, (a,b)-> (int)b[1]-(int)a[1]);
+        Arrays.sort(people,(a,b)->(int)b[1]-(int)a[1]);
         String[] result=new String[names.length];
         for(int i=0;i<names.length;i++){
             result[i]=(String)people[i][0];
