dd42c2f91d7497b4c9b2158fb95104c3fbba250d
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-18 13:21:28 +0530

    File updated at 10/18/2025, 1:16:38 PM

--- 860-LemonadeChange/860-LemonadeChange.java
+++ 860-LemonadeChange/860-LemonadeChange.java
@@ -1,28 +1,12 @@
-// Last updated: 10/18/2025, 11:54:35 AM
+// Last updated: 10/18/2025, 1:16:38 PM
 class Solution {
-    public boolean lemonadeChange(int[] bills) {
-        int five=0;
-        int ten=0;
-        for(int i=0;i<bills.length;i++){
-            if(bills[i]==5){
-                five++;
-            }else if(bills[i]==10){
-                if(five>=1){
-                    five--;
-                    ten++;
-                }else{
-                    return false;
-                }
-            }else{
-            if(ten>=1 && five>=1){
-                ten--;
-                five--;
-            }else if(five>=3){
-                five-=3;
-            }else{
+    public boolean canJump(int[] nums) {
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max){
                 return false;
             }
-        }
+            max=Math.max(max,i+nums[i]);
         }
         return true;
     }
