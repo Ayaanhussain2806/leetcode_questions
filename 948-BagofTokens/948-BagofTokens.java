4e1b9e32a3aad383379e90924a67dcbc4eb2da79
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-19 22:09:08 +0530

    File updated at 10/19/2025, 10:04:18 PM

--- 948-BagofTokens/948-BagofTokens.java
+++ 948-BagofTokens/948-BagofTokens.java
@@ -1,15 +1,25 @@
-// Last updated: 10/18/2025, 4:33:41 PM
+// Last updated: 10/19/2025, 10:04:18 PM
 class Solution {
-    public int brokenCalc(int startValue, int target) {
-        int steps=0;
-        while(target>startValue){
-            if(target%2==0){
-                target/=2;
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int l=0;
+        int r=tokens.length-1;
+        int score=0;
+        int max=0;
+        while(l<=r){
+            if(tokens[l]<=power){
+                power-=tokens[l];
+                score++;
+                l++;
+                max=Math.max(score,max);
+            }else if(score>=1){
+                power+=tokens[r];
+                score--;
+                r--;
             }else{
-                target+=1;
+                return max;
             }
-            steps++;
         }
-        return steps+(startValue-target);
+        return max;
     }
 }
