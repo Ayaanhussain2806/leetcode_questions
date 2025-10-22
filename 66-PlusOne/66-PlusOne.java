4c33d1247ed4130bc0786cbd6897dffdd6712665
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-22 22:41:06 +0530

    File updated at 10/22/2025, 10:36:16 PM

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,16 +1,15 @@
-// Last updated: 10/22/2025, 10:28:01 PM
+// Last updated: 10/22/2025, 10:36:16 PM
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
                 digits[i]++;
-                return digits; 
+                return digits;
             }
-            digits[i] = 0;  
+            digits[i]=0;
         }
-
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
+        int[] result=new int[digits.length+1];
+        result[0]=1;
         return result;
     }
 }
