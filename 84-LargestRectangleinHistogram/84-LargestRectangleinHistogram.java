5b9cd09f17d7a53ff902167d0ff323c54a6515fa
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-11-15 22:39:25 +0530

    File updated at 11/15/2025, 10:34:27 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,37 +1,26 @@
-// Last updated: 11/15/2025, 10:17:12 PM
+// Last updated: 11/15/2025, 10:34:27 PM
 class Solution {
-
-
-    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        int[] nge = new int[nums2.length];
-
-        // Step 1: Find NGE for nums2 (exactly same logic as your NGE(arr))
-        for (int i = 0; i < nums2.length; i++) {
-            while (!st.isEmpty() && nums2[i] > nums2[st.peek()]) {
-                nge[st.pop()] = nums2[i];
-            }
-            st.push(i);
-        }
-
-        // Step 2: For remaining indexes, no greater element exists
-        while (!st.isEmpty()) {
-            nge[st.pop()] = -1;
-        }
-
-        // Step 3: For nums1, find index in nums2 and print its NGE
-        int[] res = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            res[i] = -1; // default value
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    res[i] = nge[j];
-                    break;
-                }
+    public int calPoints(String[] operations) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("C")){
+                st.pop();
+            }else if(operations[i].equals("D")){
+                int d=st.peek();
+                st.push(d*2);
+            }else if(operations[i].equals("+")){
+                int h=st.pop();
+                int f=st.peek();
+                st.push(h);
+                st.push(h+f);
+            }else{
+                st.push(Integer.parseInt(operations[i]));
             }
         }
-
-        return res;
+        int sum=0;
+        for(int x:st){
+            sum+=x;
+        }
+        return sum;
     }
 }
-
