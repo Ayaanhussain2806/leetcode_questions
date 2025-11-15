304187b7632113aa5369cf0cdd50ce0f1b3c8fbd
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-11-15 22:22:10 +0530

    File updated at 11/15/2025, 10:17:12 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,29 +1,37 @@
-// Last updated: 11/15/2025, 8:07:07 PM
+// Last updated: 11/15/2025, 10:17:12 PM
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        return NGE(nums1,nums2);
-    }
-    public static int[] NGE(int[] nums1,int[] nums2){
-        int[] ans=new int[nums2.length];
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<nums2.length;i++){
-            while(!st.isEmpty() && nums2[i]>nums2[st.peek()]){
-                ans[st.pop()]=nums2[i];
+
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[nums2.length];
+
+        // Step 1: Find NGE for nums2 (exactly same logic as your NGE(arr))
+        for (int i = 0; i < nums2.length; i++) {
+            while (!st.isEmpty() && nums2[i] > nums2[st.peek()]) {
+                nge[st.pop()] = nums2[i];
             }
             st.push(i);
         }
-            while(!st.isEmpty()){
-                ans[st.pop()]=-1;
-            }
-            
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    nums1[i]=ans[j];
+
+        // Step 2: For remaining indexes, no greater element exists
+        while (!st.isEmpty()) {
+            nge[st.pop()] = -1;
+        }
+
+        // Step 3: For nums1, find index in nums2 and print its NGE
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = -1; // default value
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    res[i] = nge[j];
                     break;
                 }
             }
         }
-        return nums1;
+
+        return res;
     }
 }
+
