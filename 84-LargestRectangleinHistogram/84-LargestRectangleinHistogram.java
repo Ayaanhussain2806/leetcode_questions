132ec321b7af7544580248a8da3a3356bb45c954
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-11-25 23:50:24 +0530

    File updated at 11/25/2025, 11:45:25 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.java
@@ -1,51 +1,37 @@
-// Last updated: 11/15/2025, 11:34:40 PM
-class MyQueue {
-    Stack<Integer> input=new Stack<>();
-    Stack<Integer> output=new Stack<>();
-    int peekEle=-1;
-    public MyQueue() {
-        
+// Last updated: 11/25/2025, 11:45:25 PM
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        return Largest_Histogram(heights);
     }
-    
-    public void push(int x) {
-        if(input.isEmpty()){
-            peekEle=x;
+    public static int Largest_Histogram(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+                int h = arr[st.pop()];
+                int r = i;
+                if (st.isEmpty()) {
+                    ans = Math.max(h * r, ans);
+                } else {
+                    int l = st.peek();
+                    int area = h * (r - l - 1);
+                    ans = Math.max(area, ans);
+                }
+            }
+            st.push(i);
         }
-        input.push(x);
-    }
-    
-    public int pop() {
-        if(output.isEmpty()){
-            while(!input.isEmpty()){
-                output.push(input.peek());
-                input.pop();
+        int r = arr.length;
+        while (!st.isEmpty()) {
+            int h = arr[st.pop()];
+
+            if (st.isEmpty()) {
+                ans = Math.max(h * r, ans);
+            } else {
+                int l = st.peek();
+                int area = h * (r - l - 1);
+                ans = Math.max(area, ans);
             }
         }
-        int val=output.peek();
-        output.pop();
-        return val;
-    }
-    
-    public int peek() {
-        if(output.isEmpty()){
-            return peekEle;
-        }
-        return output.peek();
-    }
-    
-    public boolean empty() {
-        if(input.isEmpty() && output.isEmpty()){
-            return true;
-        }
-        return false;
+        return ans;
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
