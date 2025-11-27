c8f62c0243eb998d5f8d4caf1dfd144b4a63bdbb
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-11-27 11:45:53 +0530

    Create

---
+++ problem-1764223852855/problem-1764223852855.java
@@ -0,0 +1,14 @@
+// Last updated: 11/27/2025, 11:40:52 AM
+1class Solution {
+2    public int countPairs(int[] nums, int k) {
+3        int count=0;
+4        for(int i=0;i<nums.length-1;i++){
+5            for(int j=i+1;j<nums.length;j++){
+6                if(nums[i]==nums[j] && (i*j)%k==0){
+7                    count++;
+8                }
+9            }
+10        }
+11        return count;
+12    }
+13}
