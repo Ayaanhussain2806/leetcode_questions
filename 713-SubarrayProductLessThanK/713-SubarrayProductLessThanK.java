d1014d4756c9720f81b04b889e231c50c26c6ec5
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-12-02 05:19:31 +0530

    File updated at 12/2/2025, 5:14:30 AM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.java
@@ -1,25 +1,21 @@
-// Last updated: 12/2/2025, 4:56:09 AM
+// Last updated: 12/2/2025, 5:14:30 AM
 1class Solution {
-2    public int numIslands(char[][] grid) {
-3        int count=0;
-4        for(int i=0;i<grid.length;i++){
-5            for(int j=0;j<grid[0].length;j++){
-6                if(grid[i][j]=='1'){
-7                    count++;
-8                    dfs(grid,i,j);
-9                }
-10            }
-11        }
-12        return count;
-13    }
-14    public static void dfs(char[][] grid,int i,int j){
-15        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0'){
-16            return ;
-17        }
-18        grid[i][j]='0';
-19        dfs(grid,i-1,j);
-20        dfs(grid,i+1,j);
-21        dfs(grid,i,j-1);
-22        dfs(grid,i,j+1);
-23    }
-24}
+2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+3        int oldcolor=image[sr][sc];
+4        if(oldcolor==color){
+5            return image;
+6        }
+7        dfs(image,sr,sc,oldcolor,color);
+8        return image;
+9    }
+10    public static void dfs(int[][] image,int i,int j,int oldcolor,int color){
+11        if(i<0 || j<0 || i>=image.length || j>=image[0].length || image[i][j]!=oldcolor){
+12            return ;
+13        }
+14        image[i][j]=color;
+15        dfs(image,i-1,j,oldcolor,color);
+16        dfs(image,i+1,j,oldcolor,color);
+17        dfs(image,i,j-1,oldcolor,color);
+18        dfs(image,i,j+1,oldcolor,color);
+19    }
+20}
