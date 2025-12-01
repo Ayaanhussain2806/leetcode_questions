7dd7211511e4068bdf7bcd472f7a728e0ade7cfe
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-12-02 03:44:20 +0530

    File updated at 12/2/2025, 3:39:20 AM

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,24 +1,23 @@
-// Last updated: 8/11/2025, 2:17:12 PM
-class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        return Product(nums);
-    }
-    public static int[] Product(int[] nums){
-        int n=nums.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
-        left[0]=1;
-        for(int i=1;i<n;i++){
-            left[i]=left[i-1]*nums[i-1];
-        }
-        right[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            right[i]=right[i+1]*nums[i+1];
-        }
-        int[] product=new int[n];
-        for(int i=0;i<n;i++){
-            product[i]=left[i]*right[i];
-        }
-        return product;
-    }
-}
+// Last updated: 12/2/2025, 3:39:20 AM
+1class Solution {
+2    public int trap(int[] height) {
+3        return trapping(height);
+4    }public static int trapping(int[] height){
+5        int n=height.length;
+6        int[] left=new int[n];
+7        int[] right=new int[n];
+8        left[0]=height[0];
+9        for(int i=1;i<n;i++){
+10            left[i]=Math.max(left[i-1],height[i]);
+11        }
+12        right[n-1]=height[n-1];
+13        for(int i=n-2;i>=0;i--){
+14            right[i]=Math.max(right[i+1],height[i]);
+15        }
+16        int sum=0;
+17        for(int i=0;i<n;i++){
+18            sum+=Math.min(left[i],right[i])-height[i];
+19        }
+20        return sum;
+21    }
+22}
