b871a095d353dd3aa9933d58ea9bcfed7ba51509
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-12-06 22:51:14 +0530

    File updated at 12/6/2025, 10:46:14 PM

--- 16-3SumClosest/16-3SumClosest.java
+++ 16-3SumClosest/16-3SumClosest.java
@@ -1,23 +1,23 @@
-// Last updated: 11/7/2025, 10:16:40 PM
-class Solution {
-    public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-        int min=Integer.MAX_VALUE;
-        for(int k=0;k<nums.length;k++){
-            int i=k+1;
-            int j=nums.length-1;
-            while(i<j){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(Math.abs(target-sum)<Math.abs(target-min)){
-                    min=sum;
-                }
-                if(target>sum){
-                    i++;
-                }else{
-                    j--;
-                }
-            }
-        }
-        return min;
-    }
-}
+// Last updated: 12/6/2025, 10:46:14 PM
+1class Solution {
+2    public int threeSumClosest(int[] nums, int target) {
+3        Arrays.sort(nums);
+4        int min=Integer.MAX_VALUE;
+5        for(int k=0;k<nums.length;k++){
+6            int i=k+1;
+7            int j=nums.length-1;
+8            while(i<j){
+9                int sum=nums[i]+nums[j]+nums[k];
+10                if(Math.abs(target-sum)<Math.abs(target-min)){
+11                    min=sum;
+12                }
+13                if(target>sum){
+14                    i++;
+15                }else{
+16                    j--;
+17                }
+18            }
+19        }
+20        return min;
+21    }
+22}
