0c231eee9031ef2abf61d24f4a7178098c9bca44
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:20:44 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.java
@@ -0,0 +1,25 @@
+// Last updated: 2/10/2026, 3:15:31 PM
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] memo = new int[n][n];
+        // Initialize with a special value to indicate "uncomputed"
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                memo[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        return helper(nums, 0, n - 1, memo) >= 0;
+    }
+
+    private int helper(int[] nums, int i, int j, int[][] memo) {
+        if (i == j) return nums[i];
+        if (memo[i][j] != Integer.MIN_VALUE) return memo[i][j];
+
+        int pickLeft = nums[i] - helper(nums, i + 1, j, memo);
+        int pickRight = nums[j] - helper(nums, i, j - 1, memo);
+
+        memo[i][j] = Math.max(pickLeft, pickRight);
+        return memo[i][j];
+    }
+}
