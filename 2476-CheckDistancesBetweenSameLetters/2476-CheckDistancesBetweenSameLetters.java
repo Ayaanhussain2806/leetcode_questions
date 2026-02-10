2f4ebb2b9794700d8126c94b332a38a7e083baea
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:16:59 +0530

    Create 2476-CheckDistancesBetweenSameLetters

---
+++ 2476-CheckDistancesBetweenSameLetters/2476-CheckDistancesBetweenSameLetters.java
@@ -0,0 +1,19 @@
+// Last updated: 2/10/2026, 3:11:46 PM
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int[] arr=new int[26];
+        Arrays.fill(arr,-1);
+            for(int i=0;i<s.length();i++){
+                int ch=s.charAt(i)-'a';
+                if(arr[ch]==-1){
+                    arr[ch]=i;
+                }else{
+                    int dist=i-arr[ch]-1;
+                    if(dist!=distance[ch]){
+                        return false;
+                    }
+                }
+            }
+        return true;
+    }
+}
