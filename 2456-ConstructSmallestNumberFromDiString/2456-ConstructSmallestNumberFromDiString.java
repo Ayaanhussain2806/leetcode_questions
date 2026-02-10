0636c9a6649175630f25702606320e82301ecfcf
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:17:00 +0530

    Create 2456-ConstructSmallestNumberFromDiString

---
+++ 2456-ConstructSmallestNumberFromDiString/2456-ConstructSmallestNumberFromDiString.java
@@ -0,0 +1,29 @@
+// Last updated: 2/10/2026, 3:11:47 PM
+class Solution {
+    public String smallestNumber(String pattern) {
+        return construct(pattern);
+    }
+    public static String construct(String s){
+        int n=s.length();
+        int[] ans=new int[n+1];
+        int c=1;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<=n;i++){
+            if(i==n || s.charAt(i)=='I'){
+                ans[i]=c;
+                c++;
+                while(!st.isEmpty()){
+                    ans[st.pop()]=c;
+                    c++;
+                }
+            }else{
+                st.push(i);
+            }
+        }
+        String str="";
+        for(int i=0;i<ans.length;i++){
+            str+=ans[i];
+        }
+        return str;
+    }
+}
