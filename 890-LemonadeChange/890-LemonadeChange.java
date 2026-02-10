f87c12b3342bc69f624530629e4d1c747937debe
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:19:57 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,29 @@
+// Last updated: 2/10/2026, 3:14:44 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                five++;
+            }else if(bills[i]==10){
+                if(five>=1){
+                    five--;
+                    ten++;
+                }else{
+                    return false;
+                }
+            }else{
+            if(ten>=1 && five>=1){
+                ten--;
+                five--;
+            }else if(five>=3){
+                five-=3;
+            }else{
+                return false;
+            }
+        }
+        }
+        return true;
+    }
+}
