7317fc95060af8d23bcd4b735c101dc7a9cbf03a
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:20:52 +0530

    Create 438-FindAllAnagramsInAString

---
+++ 438-FindAllAnagramsInAString/438-FindAllAnagramsInAString.java
@@ -0,0 +1,34 @@
+// Last updated: 2/10/2026, 3:15:39 PM
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int n = s.length(), m = p.length();
+        if (m > n) return result;
+
+        int[] countP = new int[26]; 
+        int[] countS = new int[26]; 
+
+        for (char c : p.toCharArray()) {
+            countP[c - 'a']++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            countS[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(countP, countS)) {
+            result.add(0);
+        }
+
+        for (int i = m; i < n; i++) {
+            countS[s.charAt(i) - 'a']++;     
+            countS[s.charAt(i - m) - 'a']--;  
+
+            if (Arrays.equals(countP, countS)) {
+                result.add(i - m + 1);
+            }
+        }
+
+        return result;
+    }
+}
