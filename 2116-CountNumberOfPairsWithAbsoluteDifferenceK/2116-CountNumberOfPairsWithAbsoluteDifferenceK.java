9c816d81319203d179deaa431b06e608a6b365bf
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:17:43 +0530

    Create 2116-CountNumberOfPairsWithAbsoluteDifferenceK

---
+++ 2116-CountNumberOfPairsWithAbsoluteDifferenceK/2116-CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -0,0 +1,14 @@
+// Last updated: 2/10/2026, 3:12:30 PM
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(Math.abs(nums[i]-nums[j])==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
