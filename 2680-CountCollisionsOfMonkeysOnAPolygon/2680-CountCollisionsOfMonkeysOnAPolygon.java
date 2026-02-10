c8f15e9da98da172b622da0be0236591a39d9df0
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:16:43 +0530

    Create 2680-CountCollisionsOfMonkeysOnAPolygon

---
+++ 2680-CountCollisionsOfMonkeysOnAPolygon/2680-CountCollisionsOfMonkeysOnAPolygon.java
@@ -0,0 +1,19 @@
+// Last updated: 2/10/2026, 3:11:30 PM
+class Solution {
+    int mod = 1000000007;
+    public int monkeyMove(int n) {
+        long res = helper(2,n);
+        return (int)((res-2+mod)%mod);
+    }
+    public long helper(int base, int n){
+        if(n == 0){
+            return 1;
+        }
+        long h = helper(base,n/2);
+        long res = (h*h)%mod;
+        if(n % 2 == 1){
+            res = (res*base)%mod;
+        }
+        return res;
+    }
+}
