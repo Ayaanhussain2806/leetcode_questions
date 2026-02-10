d6d27b509563826a125cd0873024a06188e5dea7
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:17:34 +0530

    Create 2177-CheckWhetherTwoStringsAreAlmostEquivalent

---
+++ 2177-CheckWhetherTwoStringsAreAlmostEquivalent/2177-CheckWhetherTwoStringsAreAlmostEquivalent.java
@@ -0,0 +1,21 @@
+// Last updated: 2/10/2026, 3:12:20 PM
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int[] f1=new int[26];
+        int[] f2=new int[26];
+        for(int i=0;i<word1.length();i++){
+            int ch=word1.charAt(i)-'a';
+            f1[ch]++;
+        }
+        for(int i=0;i<word2.length();i++){
+            int ci=word2.charAt(i)-'a';
+            f2[ci]++;
+        }
+        for(int i=0;i<26;i++){
+            if(Math.abs(f1[i]-f2[i])>3){
+                return false;
+            }
+        }
+        return true;
+    }
+}
