adbb823485892f167ca408f93b1ceea741a4b9a7
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-02-10 15:20:15 +0530

    Create 695-MaxAreaOfIsland

---
+++ 695-MaxAreaOfIsland/695-MaxAreaOfIsland.java
@@ -0,0 +1,25 @@
+// Last updated: 2/10/2026, 3:15:02 PM
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int max=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    int ans=dfs(grid,i,j);
+                    max=Math.max(max,ans);
+                }
+            }
+        }
+        return max;
+    }
+    private int dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j]=0;
+        return 1 + dfs(grid,i-1,j)
+                + dfs(grid,i+1,j)
+         + dfs(grid,i,j-1)
+         + dfs(grid,i,j+1);
+    }
+}
